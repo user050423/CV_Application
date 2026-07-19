383242a966a6d1f50a742213ae351bbb2511e7b9
Ynotkokonut <naijabami@gmail.com>
2026-07-20 01:19:01 +0800

    Add JobEntry subcomponent to add multiple job experience entries

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-//import { useState } from 'react'
 import Personal from './Components/Personal.jsx'
 import Education from './Components/Education.jsx'
 import Experience from './Components/Experience.jsx'
@@ -7,9 +6,9 @@ import './App.css'
 function App(){
   return(
     <>
-      
+      <Personal />
       <Education />
-      
+      <Experience />      
     </>
   )
 }
